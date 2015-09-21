b878b683cec430cbabb791b47508c503a0862bf5
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-21 00:13:49 -0700

    testing comment chnage

---
+++ src/main/java/appa/ServiceA.java
@@ -0,0 +1,19 @@
+package appa;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@Stateless
+@LocalBean
+@WebService(serviceName = "ServiceAService", portName = "ServiceAPort", endpointInterface = "appa.IServiceA", targetNamespace = "http://appa/ServiceA")
+
+public class ServiceA implements IServiceA {
+
+	@Override
+	public int plus(int left, int right) {
+		// TODO Auto-generated method stub
+		return left + right; 
+		//return 0;
+	}
+
+}
